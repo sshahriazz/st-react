a84a174165c8a8a189f25908d58b419dea214f08
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2022-10-22 21:19:14 +0600

    ssr, csr supported stitches js design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createTheme, styled } from "./theme";
+import ThemeProvider from "./theme/theme-provider";
+
+const StyledButton = styled("button", {
+  backgroundColor: "$success",
+  borderRadius: "9999px",
+  fontSize: "13px",
+  lineHeight: "1",
+  fontWeight: 500,
+  paddingTop: "10px",
+  color: "$successSolidContrast",
+  paddingBottom: "10px",
+  paddingLeft: "16px",
+  paddingRight: "16px",
+  border: "0",
+  variants: {
+    color: {
+      red: { color: "$primarySolidContrast" },
+    },
+  },
+
+  // Pseudo-class selector
+  ":hover": {
+    transform: "translateY(-2px)",
+    boxShadow: "0 10px 25px rgba(0, 0, 0, .3)",
+  },
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+});
+const lightTheme = createTheme({
+  type: "light",
+});
 
 function App() {
+  const [dark, setDark] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider
+      disableBaseline={false}
+      theme={dark ? darkTheme : lightTheme}
+    >
+      <div className="App">
+        <StyledButton onClick={() => setDark((value) => !value)}>
+          Hello
+        </StyledButton>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/utils/with-defaults.ts
@@ -0,0 +1,14 @@
+import React from "react";
+
+const withDefaults = <P, DP>(
+  component: React.ComponentType<P>,
+  defaultProps: DP
+) => {
+  type Props = Partial<DP> & Omit<P, keyof DP>;
+  //@ts-ignore
+  component.defaultProps = defaultProps;
+
+  return component as React.ComponentType<Props>;
+};
+
+export default withDefaults;
